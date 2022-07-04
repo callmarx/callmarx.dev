06309b069b5ba72d8bf2b6394f8d6aad5c2e40c3
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2022-07-04 13:09:59 -0300

    fix(syntax-hightlight): add postcss-import
     - Add postcss-import (npm package)
     - Replace '@tailwind' for ''@import' in main.css
     - Add Native syntax-hightlight theme.

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    require('postcss-import'), // must be first! Otherwise syntax highlight don't work
     require('tailwindcss'),
     require('autoprefixer'),
     ...(process.env.JEKYLL_ENV == 'production' ? [require('cssnano')({ preset: 'default' })] : [])
