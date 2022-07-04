a82cf892861d3d9aba7d2c6646ce4521d8595d4c
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2022-07-04 16:01:06 -0300

    fix(custom-color): is triggering @apply error
     - need to fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
     './*.html',
   ],
   theme: {
+    colors: {
+      'beige' : { // source: https://www.colorhexa.com/f1ece4
+        100: '#f8f5f1',
+        200: '#f1eae4',
+        300: '#eae3d7',
+        400: '#e4daca',
+        500: '#ddd1bd',
+        600: '#d6c8b0',
+        700: '#d0bfa3',
+        800: '#c9b596',
+        900: '#c2ac8a',
+      }
+    },
     theme: {
       extend: {},
     },
